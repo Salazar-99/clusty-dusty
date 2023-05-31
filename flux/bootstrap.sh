cc3b38a0f6271a6a4ca531f2d1f6e8eb6d0d5389
Gerardo Salazar <Drifta10.gs@gmail.com>
2023-05-30 20:04:07 -0500

    removing flux-system

--- flux/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Requires PAT to be set as GITHUB_TOKEN environment variable
-flux bootstrap github \
-  --owner=Salazar-99 \
-  --repository=clusty-dusty \
-  --path=flux/ \
-  --personal
